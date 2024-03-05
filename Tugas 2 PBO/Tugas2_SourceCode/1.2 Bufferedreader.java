df4c125aac2ac11af7ef2a3af695a2197748acd6
fidlyS <161168213+fidlyS@users.noreply.github.com>
2024-03-05 23:11:31 +0800

    Add files via upload

---
+++ Tugas 2 PBO/Tugas2_SourceCode/1.2 Bufferedreader.java
@@ -0,0 +1,36 @@
+/*
+NIM     : 130202200094
+Nama    : Fidly Saputra Awal
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bufferedreader1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufrr = new BufferedReader(new InputStreamReader(System.in));
+        // Input
+        System.out.println("=========================================");
+        System.out.println("Silahkan isi data mahasiswa dibawah ini !");
+        System.out.println("=========================================");
+        System.out.print("Nim      : ");
+        String nim = bufrr.readLine();
+        System.out.print("Nama     : ");
+        String nama = bufrr.readLine();
+        System.out.print("Jurusan  : ");
+        String jurusan = bufrr.readLine();
+        System.out.print("Fakultas : ");
+        String fakultas = bufrr.readLine();
+
+        // Output
+        System.out.println("================================");
+        System.out.println("Data mahasiswa yang dimasukkan: ");
+        System.out.println("================================");
+        System.out.println("Nim      : " + nim);
+        System.out.println("Nama     : " + nama);
+        System.out.println("Jurusan  : " + jurusan);
+        System.out.println("Fakultas : " + fakultas);
+        
+        bufrr.close();
+    }
+}
